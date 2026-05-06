a7dc2ef51819f513dc4d5ed24a8cd81cba1e946c
IgamiLia <alejandrofuego27@gmail.com>
2026-05-06 19:22:54 -0300

    add SectionPortal & MusicButton

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,17 @@
-// @ts-check
 import { defineConfig } from "astro/config";
+import { fileURLToPath } from "url";
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     resolve: {
       alias: {
-        "@components": "/src/components",
-        "@layouts": "/src/layouts",
-        "@styles": "/src/styles",
-        "@lib": "/src/lib",
-        "@pages": "/src/pages",
+        "@components": fileURLToPath(
+          new URL("./src/components", import.meta.url),
+        ),
+        "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
+        "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
+        "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
       },
     },
   },
