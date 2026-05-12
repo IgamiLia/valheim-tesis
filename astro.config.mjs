eb84dbb41bbbbe0214581184fa1e4d22d6e091a3
IgamiLia <alejandrofuego27@gmail.com>
2026-05-12 18:33:28 -0300

    agregado de musica

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ export default defineConfig({
           new URL("./src/components", import.meta.url),
         ),
         "@layouts": fileURLToPath(new URL("./src/layouts", import.meta.url)),
+        "@scripts": fileURLToPath(new URL("./src/scripts", import.meta.url)),
         "@styles": fileURLToPath(new URL("./src/styles", import.meta.url)),
         "@lib": fileURLToPath(new URL("./src/lib", import.meta.url)),
         "@pages": fileURLToPath(new URL("./src/pages", import.meta.url)),
